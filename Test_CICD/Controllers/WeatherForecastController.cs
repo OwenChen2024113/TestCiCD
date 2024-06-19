15aa251f45d768d93bd88be4573658a7325c3de8
OhyaOwen <167050924+OwenChen2024113@users.noreply.github.com>
2024-06-19 08:53:54 +0800

    hello test 6

--- Test_CICD/Controllers/WeatherForecastController.cs
+++ Test_CICD/Controllers/WeatherForecastController.cs
@@ -33,7 +33,7 @@ public class WeatherForecastController : ControllerBase
     [HttpPost(Name = "Hello")]
     public string Post()
     {
-        return "Hello Test 5";
+        return "Hello Test 6";
     }
 }
 
